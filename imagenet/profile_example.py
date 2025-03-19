8733a6616da9505e56d8b6558ce4c782d94706c7
Timothy Chang <tyc2118@columbia.edu>
2025-03-19 13:51:46 -0700

    execute profile using loop

--- imagenet/profile_example.py
+++ imagenet/profile_example.py
@@ -1,10 +1,8 @@
 from main import main
 
-try:
-    main(['-a', 'alexnet', '--dummy', '-t', '10'])
-except TimeoutError as e:
-    print(f"Timeout occurred while running alexnet:\n{str(e)}")
-try:
-    main(['-a', 'resnet18', '--dummy', '-t', '10'])
-except TimeoutError as e:
-    print(f"Timeout occurred while running resnet18:\n{str(e)}")
+model_name = ['alexnet', 'resnet18']
+for name in model_name:
+    try:
+        main(['-a', name, '--dummy', '-t', '10'])
+    except TimeoutError as e:
+        print(f"Timeout occurred:\n{str(e)}")
